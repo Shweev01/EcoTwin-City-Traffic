b34feabc9303c14037d07b1f5793a053e95a8adc
AsmitYadav18 <asmityadavgts56@gmail.com>
2026-09-11 21:52:46 +0530

    Configure CORS for React frontend

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app= FastAPI()
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["*"], 
+    allow_origins=["http://localhost:5173"], 
     allow_methods=["*"], 
     allow_headers=["*"]
 )
